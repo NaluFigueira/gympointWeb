86e9a8c902e1a968d39a84cafa76a8b3d0ba120a
NaluFigueira <anachfigueira@gmail.com>
2019-12-31 15:11:21 -0300

    Estilização do sign in pronta

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,45 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
 
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+import logo from '~/assets/logo.png';
 
 export default function SignIn() {
-  return <div />;
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email('Insira um e-mail válido!')
+      .required('E-mail é obrigatório!'),
+    password: Yup.string().required('Senha é obrigatória'),
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  function handleSubmit({ email, password }) {
+    setLoading(true);
+    // eslint-disable-next-line no-console
+    console.log({ email, password });
+    setLoading(false);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          Seu e-mail:
+          <Input name="email" type="email" id="email" />
+        </label>
+        <label htmlFor="password">
+          Sua senha:
+          <Input name="password" type="password" id="password" />
+        </label>
+
+        <button type="submit">{loading ? 'Carregando...' : 'Acessar'}</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
+  );
 }
--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -4,7 +4,7 @@ import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: linear-gradient(-90deg, #7159c1, #ab59c1);
+  background: #ee4d64;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -12,24 +12,38 @@ export const Wrapper = styled.div`
 
 export const Content = styled.div`
   width: 100%;
-  max-width: 315px;
+  max-width: 360px;
   text-align: center;
+  padding: 30px;
+  background-color: white;
+  border-radius: 4px;
 
   form {
     display: flex;
     flex-direction: column;
+    align-items: left;
     margin-top: 30px;
 
-    input {
-      background: rgba(0, 0, 0, 0.1);
-      border: 0;
-      height: 44px;
-      padding: 0 15px;
-      color: #fff;
-      margin: 0 0 10px;
+    label {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      font-weight: bold;
+      text-transform: uppercase;
+      color: #444;
 
-      &::placeholder {
-        color: rgba(255, 255, 255, 0.7);
+      input {
+        border: 1px solid #ddd;
+        border-radius: 4px;
+        height: 45px;
+        padding: 0 15px;
+        color: #999;
+        width: 100%;
+        margin: 10px 0 20px 0;
+
+        &::placeholder {
+          color: #444444;
+        }
       }
     }
 
@@ -43,7 +57,7 @@ export const Content = styled.div`
     button {
       margin: 5px 0 0;
       height: 44px;
-      background: #3b9eff;
+      background: #f64c75;
       font-weight: bold;
       color: #fff;
       border: 0;
@@ -52,7 +66,7 @@ export const Content = styled.div`
       transition: background 0.2s;
 
       &:hover {
-        background: ${darken(0.03, '#3b9eff')};
+        background: ${darken(0.03, '#f64c75')};
       }
     }
 
